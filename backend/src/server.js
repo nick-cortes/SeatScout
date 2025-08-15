dc2b5bdf06f4a309168416c63b5fc2a803b02a46
Nicholes Cortes <187998850+ncortes9@users.noreply.github.com>
2025-08-14 22:33:54 -0400

    install cron and add folders

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,11 @@
 import express from "express";
+import cron from "node-cron";
 
 const app = express();
+
+// cron.schedule('1,10,20,30 * * * * *', () => {
+//     console.log("Current second", new Date().getSeconds());
+// })
 
 app.listen(5001, () => {
     console.log("Server started on PORT: 5001");
